a9b6d2022529d470e58c99fbd2e087f207a0239d
Ivan Kanashov <i.kanashov@tinkoff.ru>
2021-12-14 23:33:23 +0500

    feat: solve day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -23,6 +23,51 @@ def day_3_task1(filename: str) -> None:
               f'power consumption {gamma_rate * epsilon_rate}')
 
 
+def day_3_task2(filename: str) -> None:
+    """Calculate life support rating of the submarine part 2"""
+    with open(filename, encoding='utf-8') as file:
+        # read and transpose data
+        data = [[int(digit) for digit in line.strip()] for line in file]
+
+        most_data = data.copy()
+        most_transpose = list(map(list, zip(*most_data)))
+        num_bits = len(most_transpose)
+        column_num = 0
+        while column_num < num_bits:
+            column = most_transpose[column_num]
+            most_bit = 1 if len(most_data) - sum(column) <= sum(column) else 0
+            most_data = [
+                line for line in most_data if line[column_num] == most_bit]
+            most_transpose = list(map(list, zip(*most_data)))
+            column_num += 1
+            if len(most_data) == 1:
+                column_num = num_bits
+
+        least_data = data.copy()
+        least_transpose = list(map(list, zip(*least_data)))
+        num_bits = len(least_transpose)
+        column_num = 0
+        while column_num < num_bits:
+            column = least_transpose[column_num]
+            least_bit = 1 if len(least_data) - sum(column) > sum(column) else 0
+            least_data = [
+                line for line in least_data if line[column_num] == least_bit]
+            least_transpose = list(map(list, zip(*least_data)))
+            column_num += 1
+            if len(least_data) == 1:
+                column_num = num_bits
+
+        o_rate = int('0b' + ''.join(list(map(str, most_data[0]))), 2)
+        co2_rate = int('0b' + ''.join(list(map(str, least_data[0]))), 2)
+
+        print(f'input filename - {filename} o_rate - {o_rate} '
+              f'co2_rate - {co2_rate} '
+              f'life support {o_rate * co2_rate}')
+
+
 if __name__ == '__main__':
     day_3_task1('test.txt')
     day_3_task1('input.txt')
+
+    day_3_task2('test.txt')
+    day_3_task2('input.txt')
